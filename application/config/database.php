342b10e2a1100e40c539fb0d6d18d270746dec77
Tim Shaw <aradnom@gmail.com>
2012-11-18 23:41:04 -0800

    Added database info

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'dev.monterey-j.com';
+$db['default']['username'] = 'nccp_admin';
+$db['default']['password'] = 'd7yt$7s2Ol9!5~742ee';
+$db['default']['database'] = 'nccp';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
